88237d6523aa6a3bec257118eb66184d27fd8984
katiafill <katiafill@mail.ru>
2023-07-19 13:35:18 +0700

    Add Logger

--- src/main/java/katiafill/task2/Main.java
+++ src/main/java/katiafill/task2/Main.java
@@ -1,7 +1,13 @@
 package katiafill.task2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    public static  final Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        logger.info("I'm writing in log");
     }
 }
