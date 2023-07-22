cd4eeca5e2980ff0ab227ba7fce1c7202fad3b30
katiafill <katiafill@mail.ru>
2023-07-22 14:11:22 +0700

    Write tests in FileHelperTest

--- src/test/java/katiafill/task2/FileHelperTest.java
+++ src/test/java/katiafill/task2/FileHelperTest.java
@@ -11,16 +11,23 @@ class FileHelperTest {
 
     @Test
     void readFile() {
-//        String testFile = "resources\\circle.txt";
-//        try {
-//            List<String> result = FileHelper.readFile(testFile);
-//            assertEquals(result, List.of("CIRCLE", "10"));
-//        } catch (IOException ex) {
-//            fail(ex);
-//        }
+        String testFile = "resources\\circle.txt";
+        try {
+            List<String> result = FileHelper.readFile(testFile);
+            assertEquals(result, List.of("CIRCLE", "10"));
+        } catch (IOException ex) {
+            fail(ex);
+        }
     }
 
     @Test
     void writeFile() {
+        String testFile = "resources\\result.txt";
+        try {
+            String text = "Test text \n2 line";
+            FileHelper.writeFile(testFile, text);
+        } catch (IOException ex) {
+            fail(ex);
+        }
     }
 }
